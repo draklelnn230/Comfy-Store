d1dae2179ee58c8c320c40ef1dc195656832e65a
Lan <h.baolan20020@gmail.com>
2022-05-03 05:18:01 +0700

    Working with Cart - Decrease Amount

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -76,6 +76,17 @@ function increaseAmount(id) {
   });
   return newAmount;
 }
+function decreaseAmount(id) {
+  let newAmount;
+  cart = cart.map((cartItem) => {
+    if (cartItem.id === id) {
+      newAmount = cartItem.amount - 1;
+      cartItem = { ...cartItem, amount: newAmount };
+    }
+    return cartItem;
+  });
+  return newAmount;
+}
 function setupCartFunctionality() {
   cartItemsDOM.addEventListener("click", function (e) {
     const element = e.target;
@@ -87,12 +98,21 @@ function setupCartFunctionality() {
       removeItem(id);
       parent.parentElement.remove();
     }
+    // increase
     if (parent.classList.contains("cart-item-increase-btn")) {
       const newAmount = increaseAmount(parentID);
       parent.nextElementSibling.textContent = newAmount;
     }
-    // increase
     // decrease
+    if (parent.classList.contains("cart-item-decrease-btn")) {
+      const newAmount = decreaseAmount(parentID);
+      if (newAmount === 0) {
+        removeItem(id);
+        parent.parentElement.parentElement.remove();
+      } else {
+        parent.previousElementSibling.textContent = newAmount;
+      }
+    }
     displayCartItemCount();
     displayCartTotal();
     setStorageItem("cart", cart);
